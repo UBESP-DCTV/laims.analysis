063354afd472f04cf3392df21f70cf237f264a99
CorradoLanera <corrado.lanera@gmail.com>
2022-02-28 22:59:58 +0100

    update run.R

--- run.R
+++ run.R
@@ -1,12 +1,18 @@
 # To view your current pipeline status, and possibly run/update it,
-# you can simply source/execute this script
+# you can simply source/execute this script (maybe using the
+# `dev/03-run_cycle.R` script)
 {
-  function(proceed = TRUE, save_all = TRUE, targets_only = TRUE) {
+  function(proceed = TRUE, save_all = TRUE) {
     if (interactive()) {
-      if (requireNamespace("rstudioapi") && save_all) {
+      if (
+        requireNamespace("rstudioapi") &&
+        rstudioapi::isAvailable() &&
+        save_all
+      ) {
         rstudioapi::documentSaveAll()
       }
-      targets::tar_visnetwork(targets_only = targets_only) |>
+
+      targets::tar_visnetwork() |>
         print()
 
       proceed <- usethis::ui_yeah(
@@ -16,11 +22,14 @@
 
     if (proceed) {
       withr::with_envvar(
-        list(RSTUDIO_VERSION = "2021.09.0"),
-        targets::tar_make()
+        list(RSTUDIO_VERSION = "2021.09.0"), {
+          # devtools::test(stop_on_failure = TRUE)
+          targets::tar_make()
+        }
       )
-      targets::tar_visnetwork(targets_only = targets_only) |>
+      targets::tar_visnetwork(targets_only = TRUE) |>
         print()
     }
   }
 }()
+
