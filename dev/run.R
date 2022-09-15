8695dc5badfa0092c7e0838065cf68de2ba76d4d
CorradoLanera <corrado.lanera@gmail.com>
2022-09-15 12:58:05 +0200

    Update and move run.R->dev/run.R

--- dev/run.R
+++ dev/run.R
@@ -15,9 +15,10 @@
       targets::tar_visnetwork() |>
         print()
 
-      proceed <- usethis::ui_yeah(
-        "Do you want to proceed with {usethis::ui_code('tar_make')}?"
-      )
+      proceed <- usethis::ui_yeah(paste0(
+        "Do you want to make your pipeline? ",
+        "({usethis::ui_code('tar_make')})"
+      ))
     }
 
     if (proceed) {
