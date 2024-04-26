48708a6e9b67a302cc5d48f63f5edf5065f30778
Corrado Lanera <corrado.lanera@gmail.com>
2024-04-26 11:30:10 +0200

    add `%||%`

--- R/utils.R
+++ R/utils.R
@@ -1,6 +1,6 @@
 
 # Here below put your small tiny supporting functions -------------
-
+`%||%` <- function(x, y) if (is.null(x)) y else x
 
 view_in_excel <- function(.data) {
   if (interactive()) {
